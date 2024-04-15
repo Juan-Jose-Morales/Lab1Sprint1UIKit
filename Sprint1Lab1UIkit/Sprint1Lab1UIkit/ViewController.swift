f2b598a8af7c742361c44e0dd230e67a76d3dc7c
JuanJose <jhonajose@hotmail.es>
2024-04-15 14:23:07 +0200

    Se ha terminado la funcionalidad del botón

--- Sprint1Lab1UIkit/Sprint1Lab1UIkit/ViewController.swift
+++ Sprint1Lab1UIkit/Sprint1Lab1UIkit/ViewController.swift
@@ -13,41 +13,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightParagraphLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var changeTextButton: UIButton!
-    var lastModifiedLabel: UILabel?
+    var lastModifiedIndex: Int = -1
+    var textIndex = 0
+    let texts = [
+            """
+                    Apple Inc. es una empresa estadounidense multinacional que diseña y produce equipos electrónicos, software y servicios en línea, con sede en Cupertino, California.
+                    Fundada por Steve Jobs, Steve Wozniak y Ronald Wayne en 1976, la empresa es conocida por diseñar, desarrollar y vender equipos electrónicos, software y servicios en línea.
+                    """,
+                    """
+                    Android es un sistema operativo móvil desarrollado por Google, basado en el núcleo Linux y diseñado principalmente para dispositivos móviles con pantalla táctil como teléfonos inteligentes y tabletas.
+                    Android fue inicialmente desarrollado por Android Inc., que Google respaldó financieramente y más tarde compró en 2005.
+                    """,
+                    """
+                    Linux es un sistema operativo de código abierto basado en UNIX que se ha convertido en una de las principales plataformas de sistemas operativos en todo el mundo.
+                    Desarrollado por una comunidad de desarrolladores de software de todo el mundo, Linux es conocido por su estabilidad, seguridad y capacidad de personalización.
+                    """,
+                    """
+                    Apple es mejor que Android
+                    """,
+                    """
+                    Android es mejor que Apple
+                    """,
+                    """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor justo vel efficitur efficitur. Phasellus auctor interdum odio, sed pellentesque metus cursus nec. Integer nec arcu ac leo ultricies ultricies. Fusce in velit et magna iaculis vehicula ut sed tortor. Ut viverra lorem sit amet urna convallis convallis. Sed semper semper urna, eu consequat turpis. Integer lobortis id metus vel bibendum. Proin eu metus eu risus tincidunt tempus. Nulla at ex quis sem vestibulum rutrum. Nam ac vehicula nisi. Aliquam sodales ante ut magna auctor, nec fringilla purus malesuada. Nulla facilisi. Maecenas facilisis efficitur mauris ac mattis. In euismod justo id urna scelerisque tempor.
+                    """
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        leftParagraphLabel.text = "Apple es mejor que Android"
-        rightParagraphLabel.text = "Android es mejor que Apple"
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .top
         imageView.image = UIImage(named: "AndroidVsApple")
         changeTextButton.setTitle("Cambiar texto", for: .normal)
         changeTextButton.addTarget(self, action: #selector(changeText), for: .touchUpInside)
+        
+        leftParagraphLabel.text = texts[0]
+        rightParagraphLabel.text = texts[1]
 
     }
     
     @IBAction func changeText (){
         
-        let  newText = RandomTextGenerator.random()
-        let labelToUpdate: UILabel
-        if let lastLabel = lastModifiedLabel{
-            
-            labelToUpdate = (lastLabel == leftParagraphLabel) ? rightParagraphLabel : leftParagraphLabel
-        }else{
-            labelToUpdate = Bool.random() ? leftParagraphLabel : rightParagraphLabel
+        let randomLabelIndex = Bool.random() ? 0 : 1
+        
+        var newTextIndex = Int.random(in: 0..<texts.count)
+        
+        if newTextIndex == lastModifiedIndex{
+            newTextIndex = (newTextIndex + 1) % texts.count
         }
-        labelToUpdate.text = newText
-        lastModifiedLabel = labelToUpdate
+        
+        if randomLabelIndex == 0{
+            leftParagraphLabel.text = texts[newTextIndex]
+        }else{
+            rightParagraphLabel.text = texts[newTextIndex]
+        }
+        
+        lastModifiedIndex = newTextIndex
     }
 }
 
-class RandomTextGenerator{
-    
-    static let text = "Linux es mejor que todo"
-    
-        static func random() -> String{
-            let startIndex = text.index(text.startIndex, offsetBy: Int.random(in: 0..<text.count/2))
-            let endIndex = text.index(text.startIndex, offsetBy: Int.random(in: text.count/2..<text.count))
-            return String(text[startIndex..<endIndex])
-        }
-}
+
